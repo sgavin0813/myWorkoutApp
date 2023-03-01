85ce7000c63f8796c989b25f6b9d2f836ad75a5e
sgavin0813 <s.gavin0813@gmail.com>
2023-03-01 12:47:57 -0500

    nav bar added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,26 +3,55 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
+
 var exerciseList = mutableListOf<ExerciseData>()
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    lateinit var toggleHamB : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navBar: NavigationView = findViewById(R.id.navBar)
+        val drawerMenu : DrawerLayout = findViewById(R.id.drawerMenu)
         initializeList()
-        val bmiButton = findViewById<Button>(R.id.bmiButton)
-        bmiButton.setOnClickListener{
-            replaceFragment(BmiFragment())
+        //this is what home fragment would be
+        replaceFragment(BmiFragment())
+        toggleHamB = ActionBarDrawerToggle(this,drawerMenu,R.string.open,R.string.close)
+        drawerMenu.addDrawerListener(toggleHamB)
+        toggleHamB.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true )
+        navBar.setNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.homeButton -> {
+                    replaceFragment(BmiFragment())
+                }
+
+                R.id.exerciseList -> {
+                    replaceFragment(ExerciseFragment())
+                }
+            }
+
+            true
         }
 
-        val exerciseListButton = findViewById<Button>(R.id.exerciseListButton)
-        exerciseListButton.setOnClickListener {
-            replaceFragment(ExerciseFragment())
+
+
+    }
+    //onOptionsItemSelected-> handle user clicks on the options menu of the app
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //checks if toggleHamB is the menuItem we have selected
+        if (toggleHamB.onOptionsItemSelected(item)){
+            return true
         }
+        return super.onOptionsItemSelected(item)
     }
     private fun initializeList(){
 
